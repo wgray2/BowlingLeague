7af6bd033453907c549bab3d3073fc5ed6c0d187
wgray2 <34471241+wgray2@users.noreply.github.com>
2021-03-29 16:38:25 -0600

    Add project files.

---
+++ Components/TeamViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using BowlingLeague.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingLeague.Components
+{
+    public class TeamViewComponent : ViewComponent
+    {
+        private BowlingLeagueContext context;
+        public TeamViewComponent(BowlingLeagueContext con)
+        {
+            context = con;
+        }
+        public IViewComponentResult Invoke()
+        {
+
+            return View(context.Teams
+                .Distinct()
+                .OrderBy(x => x));
+        }
+
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using BowlingLeague.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingLeague.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private BowlingLeagueContext context { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, BowlingLeagueContext con)
+        {
+            _logger = logger;
+            context = con;
+        }
+
+        public IActionResult Index(long? teamid, string teamname)
+        {
+            return View(context.Bowlers
+                .FromSqlInterpolated($"SELECT * FROM Bowlers WHERE TeamID = {teamid} OR {teamid} IS NULL")
+                .ToList());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
